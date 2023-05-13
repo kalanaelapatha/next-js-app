88325876881b1330ba6ea415bee9726d9f16eb43
kalanaelapatha <kalanaelapatha@gmail.com>
2023-05-13 12:09:53 +0530

    fix : "changes of layout"

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        This is the test next application which created to learn basic syntac
+        and behavior of the NextJS
+      </p>
+    </section>
+  );
 };
 
 export default Home;
